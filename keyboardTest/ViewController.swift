fb4859a6f02da1cdc83b1f9618cced8221e55af1
RyoNagai <59611366+RyoNagai-8@users.noreply.github.com>
2021-06-09 11:11:56 +0900

    Edit ViewController

--- keyboardTest/ViewController.swift
+++ keyboardTest/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var testTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        testTableView.delegate = self
+        testTableView.dataSource = self
+        
     }
 
 
+}
+
+extension ViewController: UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = testTableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        
+        cell.backgroundColor = .green
+        
+        return cell
+    }
+    
+    
 }
 
